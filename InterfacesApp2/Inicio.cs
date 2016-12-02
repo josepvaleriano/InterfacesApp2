322bf1df2a602ef33c8979a02685f0715fb3396f
josepvaleriano <joseluis.valeriano@gmail.com>
2016-12-02 15:01:58 -0600

    first commit

---
+++ InterfacesApp2/Inicio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace InterfacesApp2
+{
+	public class Inicio : ContentPage
+	{
+		public Inicio()
+		{
+			Content = new StackLayout
+			{
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+
+
+#if __IOS__
+			Padding = new Thickness(0, 25, 0, 0);
+			#endif
+		}
+	}
+}
+
---
+++ InterfacesApp2/InterfacesApp2Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace InterfacesApp2
+{
+	public partial class InterfacesApp2Page : ContentPage
+	{
+		public InterfacesApp2Page()
+		{
+			InitializeComponent();
+		}
+	}
+}
